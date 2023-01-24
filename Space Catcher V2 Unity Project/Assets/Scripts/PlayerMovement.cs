5e3856dae9b5ba0f0fd7391fa8dc0a442c99fc61
beecoding12345 <94185861+beecoding12345@users.noreply.github.com>
2023-01-24 11:31:27 +0000

    player movment done

--- Space Catcher V2 Unity Project/Assets/Scripts/PlayerMovement.cs
+++ Space Catcher V2 Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 10f;
+
+    Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xMove = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector3(xMove, 0, 0) * speed;
     }
 }
